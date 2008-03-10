96c82832f746062726919273518406c2b4e43a7b
alex <alex@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-03-10 00:30:32 +0000

    final fix to TableColor ref!

--- branches/milestone_1.x/wpi/extensions/ContributionScores/ContributionScores.php
+++ branches/milestone_1.x/wpi/extensions/ContributionScores/ContributionScores.php
@@ -45,6 +45,6 @@ function efContributionScores() {
 function efContributionScores_addHeadScripts(&$out) {
 	global $wgScriptPath;
 
-	$out->addScript( '<link rel="stylesheet" type="text/css" href="/skins/wikipathways/TableColor.css" />'. "\n" );
+	$out->addScript( "<link rel=\"stylesheet\" type=\"text/css\" href=\"$wgScriptPath/skins/wikipathways/TableColor.css\" /> \n" );
 	return true;
 }
