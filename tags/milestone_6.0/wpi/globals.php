8e274c5a60190d4e95e324da936f65b3f529cf18
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-08-21 12:36:15 +0000

    created M6 tag

---
+++ tags/milestone_6.0/wpi/globals.php
@@ -0,0 +1,37 @@
+<?php
+global $wgScriptPath;
+
+//File types
+define("FILETYPE_IMG", "svg");
+define("FILETYPE_GPML", "gpml");
+define("FILETYPE_MAPP", "mapp");
+define("FILETYPE_PNG", "png");
+
+//Script info
+$wpiPathName = 'wpi'; //pathname containing wpi script
+$wpiTmpName = 'tmp'; //temp path name
+$wpiScriptFile = 'wpi.php';
+
+$wpiScriptPath = realpath(dirname(__FILE__));
+$wpiScript = "$wpiScriptPath/$wpiScriptFile"; 
+$wpiTmpPath = "$wpiScriptPath/$wpiTmpName";
+$siteURL = "http://{$_SERVER['HTTP_HOST']}/$wgScriptPath";
+$wpiURL = "$siteURL/$wpiPathName";
+
+define("WPI_SCRIPT_PATH", $wpiScriptPath);
+define("WPI_SCRIPT", $wpiScript);
+define("WPI_TMP_PATH", $wpiTmpPath);
+define("SITE_URL", $siteURL);
+define("WPI_URL",  $wpiURL);
+define("WPI_SCRIPT_URL", WPI_URL . '/' . $wpiScriptFile);
+define("WPI_TMP_URL", WPI_URL . '/' . $wpiTmpName);
+
+//JS info
+define("JS_SRC_EDITAPPLET", $wgScriptPath . "/wpi/js/editapplet.js");
+define("JS_SRC_RESIZE", $wgScriptPath . "/wpi/js/resize.js");
+define("JS_SRC_PROTOTYPE", $wgScriptPath . "/wpi/js/prototype.js");
+
+//Users
+define("USER_MAINT_BOT", "MaintBot"); //User account for maintenance scripts
+
+?>
