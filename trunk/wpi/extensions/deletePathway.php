c07df9987cfbcc4a3ea0788767ba97170fd35ca3
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-12-12 08:52:48 +0000

    branch for M9

---
+++ trunk/wpi/extensions/deletePathway.php
@@ -0,0 +1,40 @@
+<?php
+require_once('wpi/wpi.php');
+
+/**
+ * Disables deletion for pathway pages.
+ * TODO: Disable this hook for running script to transfer to stable ids
+ */
+$wgHooks['ArticleDelete'][] = 'checkForPathway';
+
+function checkForPathway(&$article, &$user, &$reason, $error = '') {
+	if($article && !$error && $article->getTitle()->getNamespace() == NS_PATHWAY) {
+		//Prevent pathway page deletion, mark deleted instead of removing the page
+		$pathway = Pathway::newFromTitle($article->getTitle());
+		$pathway->delete($reason);
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Special user permissions once a pathway is deleted.
+ * TODO: Disable this hook for running script to transfer to stable ids
+ */
+$wgHooks['userCan'][] = 'checkDeleted';
+
+function checkDeleted($title, $user, $action, $result) {
+	if($action == 'edit' && $title->getNamespace() == NS_PATHWAY) {
+		$pathway = Pathway::newFromTitle($title);
+		if($pathway->isDeleted()) {
+			//Only users with 'delete' permission can revert deletion
+			//So disable edit for all other users
+			$result = $title->getUserPermissionsErrors('delete', $user) == array();
+			return false;
+		}
+	}
+	$result = null;
+	return true;
+}
+
+?>
