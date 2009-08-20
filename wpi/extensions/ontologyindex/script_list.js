4f1bee4457c94aa6d4d982c3557c687f06426878
chetan <chetan@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2009-08-20 15:43:25 +0000

    Added List mode!

--- wpi/extensions/ontologyindex/script_list.js
+++ wpi/extensions/ontologyindex/script_list.js
@@ -4,7 +4,7 @@ var ontologies = new Array(3);
 ontologies[0] = ["Pathway Ontology",1035,"PW:0000001"];
 ontologies[1] = ["Disease",1009,"DOID:4"];
 ontologies[2] = ["Cell Type",1006,"CL:0000000"];
-
+var filter = "All";
 var last_select = null;
 var last_select_species = "All Species";
 var species = "All Species";
@@ -17,6 +17,7 @@ addOnloadHook(
     function () {
     document.getElementById("index_container").innerHTML = "<div id='index_mode'>" +
         "<a href='" + server_url +"list'>List</a> | <a href='" + server_url +"tree'>Tree</a>" +
+        "<br> Sort by : " + "<a onClick='set_filter(\"All\");'> None </a> " + "| " + "<a onClick='set_filter(\"Edited\");'> Most Edited </a> | <a onClick='set_filter(\"Popular\");'>Popularity</a>" +
         "</div>" +
         "<div id='container_left'>" +
         "<div id='species_list'>Loading...</div>" + 
@@ -25,7 +26,7 @@ addOnloadHook(
         "</div>" +
         "<div id='container_right'>" +
         "<div id='pathway_list'></div>" +
-        "<div id='treeDiv'>Please select a top level Ontology term !</div>" +
+        "<div id='treeDiv'></div>" +
         "</div>" ;
     init_ontology_list();
     }
@@ -35,6 +36,7 @@ function init_ontology_list()
 {
    document.getElementById("ontology_list").innerHTML = "<font size='4'><b>Ontologies :</b></font><br>" ;
    get_species();
+   fetch_ontology_list(0);
 }
 
 function fetch_ontology_list(no)
@@ -51,7 +53,7 @@ function fetch_ontology_list(no)
                                         id = term.substring(term.lastIndexOf(" - ")+3,term.length);
                                         id = id.replace("||","");
                                         term = term.substring(0,term.lastIndexOf(" - "));
-                                        document.getElementById("ontology_list").innerHTML += "<li><a  id=" + id + " onClick='create_tree(\"" + term + " - " + id + "\",\"" + id + "\");'>" + term + '</a></li>';
+                                        document.getElementById("ontology_list").innerHTML += "<li><a  id=" + id + " onClick='set_ontology(\"" + term + " - " + id + "\",\"" + id + "\");'>" + term + '</a></li>';
                                         }
                                 document.getElementById("ontology_list").innerHTML += "<br>";
                                 if(no < (ontologies.length-1))
@@ -157,21 +159,44 @@ function set_species(specie)
            document.getElementById(specie).style.fontWeight = "bold";
            last_select_species = specie;
            document.getElementById("ontology_list").innerHTML = "<font size='4'><b>Ontologies :</b></font><br>" ;
-           document.getElementById("treeDiv").innerHTML = "Please select a top level Ontology term !";
            document.getElementById("pathway_list").innerHTML = "";
-           fetch_ontology_list(0);
+//           fetch_ontology_list(0);
+           get_pathways_list();
 }
-
-
-function create_tree(root_id,id)
+function get_pathways_list()
 {
 
-           var tree;
+    var handleSuccess = function(o){
 
-           tree = new YAHOO.widget.TreeView("treeDiv");
-           tree.setDynamicLoad(loadNodeData);
-           var root = tree.getRoot();
-           var aConcepts = root_id ;
+    document.getElementById("treeDiv").innerHTML = "<ul>" + o.responseText + "</ul>";
+}
+
+var handleFailure = function(o){
+	if(o.responseText !== undefined){
+		div.innerHTML = "<ul><li>Transaction id: " + o.tId + "</li>";
+		div.innerHTML += "<li>HTTP status: " + o.status + "</li>";
+		div.innerHTML += "<li>Status code message: " + o.statusText + "</li></ul>";
+	}
+}
+
+var callback =
+{
+  success:handleSuccess,
+  failure:handleFailure,
+  argument: { foo:"foo", bar:"bar" }
+};
+    if(last_select != null)
+        var sUrl = opath + "/wp_proxy.php?filter=" + filter + "&action=list&species="+ species + "&term=" + last_select;
+    else
+        var sUrl = opath + "/wp_proxy.php?filter=" + filter + "&action=list&species="+ species + "&term=";
+    document.getElementById("treeDiv").innerHTML = "Loading...";
+    var request = YAHOO.util.Connect.asyncRequest('GET', sUrl, callback);
+
+}
+    
+function set_ontology(root_id,id)
+{
+
            document.getElementById("pathway_list").innerHTML = "";
            if(last_select != null)
                 {
@@ -182,90 +207,15 @@ function create_tree(root_id,id)
            last_select = id;
            document.getElementById(id).style.color = "#FF0000";
            document.getElementById(id).style.fontWeight = "bold";
-           var tempNode = new YAHOO.widget.TextNode(aConcepts, root, tree);
-           tempNode.c_id=tempNode.label.substring(tempNode.label.lastIndexOf(" - ")+3,tempNode.label.length);
-           tempNode.label = tempNode.label.substring(0,tempNode.label.lastIndexOf(" - "));
+           get_pathways_list();
 
-           // display the pathways
-//           get_pathways(tempNode.label, tempNode.c_id);
-//
-           tree.draw();
-//
-           tree.subscribe("labelClick", function(node) {
-               if(node.c_id.lastIndexOf("0000a")>0)
-                   {
-                        var pw_url = node.c_id.replace("0000a","");
-                        winRef = window.open( wgServer + wgScript + "/Pathway:" + pw_url ,node.label);
-                   }
-//            get_pathways(node.label,node.c_id);
-//
-////            tree.destroy();
-////            tree = null;
-////            YAHOO.util.Event.onDOMReady(ontologytree.init, ontologytree,true);
-//	       })
-//
-//            tree.subscribe("expand", function(node) {
-//                get_pathways(node.label,node.c_id);
-//                // return false; // return false to cancel the expand
-         })
+}
 
-    return {
-        init: function() {
-            buildTree(root_id);
-        }
-    }
-        }
-    function loadNodeData(node, fnLoadComplete)
-    {
-
-            //Get the node's label and urlencode it; this is the word/s
-            //on which we'll search for related words:
-     		// encodeURI(node.label);
-
-            var ontology_id = get_ontology_id(node.c_id);
-            var sUrl = opath + "/wp_proxy.php?tree_pw=yes&action=tree&mode=tree&ontology_id=" + ontology_id + "&concept_id=" + encodeURI(node.c_id) + "&species=" + species;
-            var callback = {
-                success: function(oResponse) {
-                    var oResults = YAHOO.lang.JSON.parse(oResponse.responseText);
-                    if((oResults.ResultSet.Result) && (oResults.ResultSet.Result.length)) {
-                        if(YAHOO.lang.isArray(oResults.ResultSet.Result)) {
-                            for (var i=0, j=oResults.ResultSet.Result.length; i<j; i++) {
-
-                            var tempNode = new YAHOO.widget.MenuNode(oResults.ResultSet.Result[i], node, false);
-                            tempNode.c_id=tempNode.label.substring(tempNode.label.lastIndexOf(" - ")+3,tempNode.label.length);
-
-                            if(tempNode.c_id.lastIndexOf("0000a")>0 || tempNode.c_id.lastIndexOf("||")>0)
-                                {
-                                       tempNode.isLeaf = true;
-
-                                }
-                            tempNode.c_id = tempNode.c_id.replace("||","");
-                            tempNode.label =    tempNode.label.substring(0,tempNode.label.lastIndexOf(" - "));
-                            }
-                        }
-                    }
-                    oResponse.argument.fnLoadComplete();
-                },
-
-                failure: function(oResponse) {
-                    YAHOO.log("Failed to process XHR transaction.", "info", "example");
-                    oResponse.argument.fnLoadComplete();
-                },
-
-                argument: {
-                    "node": node,
-                    "fnLoadComplete": fnLoadComplete
-                },
-
-                //timeout -- if more than 7 seconds go by, we'll abort
-                //the transaction and assume there are no children:
-                timeout: 25000
-            };
-
-            YAHOO.util.Connect.asyncRequest('GET', sUrl, callback);
-        }
-
-
+function set_filter(filterName)
+{
+    filter = filterName;
+    get_pathways_list();
+}
 function get_ontology_id(tag_id)
 {
         switch(tag_id.substring(0,2))
