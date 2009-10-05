59f7115b7a1e4900bf21f7ebc5193e5f0d8451dc
chetan <chetan@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2009-10-05 14:25:41 +0000

    Fixed sidebar issue

--- branches/gsoc-ontologies/wpi/extensions/otag/otags_main.php
+++ branches/gsoc-ontologies/wpi/extensions/otag/otags_main.php
@@ -69,7 +69,7 @@ $output = <<<HTML
 </td>
 <td>
 <div id="treeDiv3" class="treeDiv"></div>
-</td></tr></table></
+</td></tr></table>
 </div>
 <link rel="stylesheet" type="text/css" href="$opath/otag.css" />
 <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/autocomplete/assets/skins/sam/autocomplete.css" />
