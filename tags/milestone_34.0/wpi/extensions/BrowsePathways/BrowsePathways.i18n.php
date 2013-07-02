097dbec771fb0ddb2b6311574ce7e07222fde641
AlexanderPico <AlexanderPico@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-07-02 17:46:16 +0000

    creating tag

---
+++ tags/milestone_34.0/wpi/extensions/BrowsePathways/BrowsePathways.i18n.php
@@ -0,0 +1,13 @@
+<?php
+$messages['en'] = array(
+	'browsepathways' => 'Browse pathways',
+	'browsepathways-select-species' => 'Species:',
+	'browsepathways-select-collection' => 'Collection:',
+	'browsepathways-select-view' => 'View:',
+	'browsepathways-all-species' => 'All Species',
+	'browsepathways-uncategorized-species' => 'Uncategorized',
+
+	'browsepathways-view-list' => 'List Mode',
+	'browsepathways-view-thumbs' => 'Thumbnail Mode',
+	'browsepathways-view-single' => 'Single Mode',
+);
---
+++ tags/milestone_34.0/wpi/extensions/CurationTags/SpecialCurationTags/SpecialCurationTags.i18n.php
@@ -0,0 +1,9 @@
+<?php
+$allMessages = array(
+	'en' => array(
+		'curationtags'      => 'Curation tags',
+		'curation-tag-show' => 'Curation tag: $1',
+		'wpict-delete'      => 'delete',
+		'wpict-too-new'     => 'recent edits',
+	)
+);
