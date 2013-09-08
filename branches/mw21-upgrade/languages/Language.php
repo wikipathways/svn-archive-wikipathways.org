4f707167bdf594e9a8263be27c28d88aa0d72c1c
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-09-08 04:20:33 +0000

    Update for E_STRICT

--- branches/mw21-upgrade/languages/Language.php
+++ branches/mw21-upgrade/languages/Language.php
@@ -1672,7 +1672,7 @@ class Language {
 	}
 
 	# Fill a MagicWord object with data from here
-	function getMagic( &$mw ) {
+	function getMagic( $mw ) {
 		if ( !$this->mMagicHookDone ) {
 			$this->mMagicHookDone = true;
 			wfRunHooks( 'LanguageGetMagic', array( &$this->mMagicExtensions, $this->getCode() ) );
