47a5d1ff441f077ac9421999cc5fdffb807ff5a9
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-09-11 21:56:22 +0000

    Enable more autoloading

--- branches/mw21-upgrade/wpi/extensions/PathwayViewer/PathwayViewer.php
+++ branches/mw21-upgrade/wpi/extensions/PathwayViewer/PathwayViewer.php
@@ -1,82 +1,67 @@
 <?php
-require_once('DetectBrowserOS.php');
 
 /*
  * Loads an interactive pathway viewer using svgweb.
  */
-$wfPathwayViewerPath = WPI_URL . "/extensions/PathwayViewer";
-
-$wgExtensionFunctions[] = 'wfPathwayViewer';
-$wgHooks['LanguageGetMagic'][]  = 'wfPathwayViewer_Magic';
-
-function wfPathwayViewer() {
-	global $wgParser;
-	$wgParser->setFunctionHook( "PathwayViewer", "displayPathwayViewer" );
-}
-
-function wfPathwayViewer_Magic( &$magicWords, $langCode ) {
-	$magicWords['PathwayViewer'] = array( 0, 'PathwayViewer' );
-	return true;
-}
-
-function displayPathwayViewer(&$parser, $pwId, $imgId) {
-	global $wgOut, $wgStylePath, $wfPathwayViewerPath, $wpiJavascriptSources, $wgScriptPath,
-		$wpiJavascriptSnippets, $jsRequireJQuery, $wpiAutoStartViewer, $wgRequest, $wgJsMimeType;
-
-	$jsRequireJQuery = true;
-
-	try {
-		$parser->disableCache();
-
-		//Force flash renderer
-		//<meta name="svg.render.forceflash" content="true">
-		$wgOut->addMeta('svg.render.forceflash', 'true');
-
-		//Add javascript dependencies
-		XrefPanel::addXrefPanelScripts();
-		$wpiJavascriptSources = array_merge($wpiJavascriptSources, PathwayViewer::getJsDependencies());
-
-		$script = "PathwayViewer_basePath = '" . $wfPathwayViewerPath . "/';";
-		$wpiJavascriptSnippets[] = $script;
-
-		$revision = $wgRequest->getval('oldid');
-		$pvPwAdded[] = $pwId . '@' . $revision;
-
-		$pathway = Pathway::newFromTitle($pwId);
-		if($revision) {
-			$pathway->setActiveRevision($revision);
-		}
-		$svg = $pathway->getFileURL(FILETYPE_IMG);
-		$gpml = $pathway->getFileURL(FILETYPE_GPML);
-
-		$dostart = ',start: true';
-		$start = $dostart; //Autostart by default
-
-		//First switch by variable in pass.php
-		if(!$wpiAutoStartViewer) $start = $dostart;
-
-		//Allow override via get parameter
-		$getStart = $wgRequest->getval('startViewer');
-		if($getStart == 'false' || $getStart == '0') $start = '';
-		if($getStart == 'true' || $getStart == '1') $start = $dostart;
-
-		$script = <<<SCRIPT
-			var viewer = new PathwayViewer({
-				  imageId: "$imgId",
-						svgUrl: "$svg",
-						gpmlUrl: "$gpml"$start
-						});
-		PathwayViewer_viewers.push(viewer);
-SCRIPT;
-		$script = "<script type=\"{$wgJsMimeType}\">" . $script . "</script>\n";
-		return array($script, 'isHTML'=>1, 'noparse'=>1);
-	} catch(Exception $e) {
-		return "invalid pathway title: $e";
-	}
-	return true;
-}
-
 class PathwayViewer {
+	static function display(&$parser, $pwId, $imgId) {
+		global $wgOut, $wgStylePath, $wfPathwayViewerPath, $wpiJavascriptSources, $wgScriptPath,
+			$wpiJavascriptSnippets, $jsRequireJQuery, $wpiAutoStartViewer, $wgRequest, $wgJsMimeType;
+
+		$jsRequireJQuery = true;
+
+		try {
+			$parser->disableCache();
+
+			//Force flash renderer
+			//<meta name="svg.render.forceflash" content="true">
+			$wgOut->addMeta('svg.render.forceflash', 'true');
+
+			//Add javascript dependencies
+			XrefPanel::addXrefPanelScripts();
+			$wpiJavascriptSources = array_merge($wpiJavascriptSources, PathwayViewer::getJsDependencies());
+
+			$script = "PathwayViewer_basePath = '" . $wfPathwayViewerPath . "/';";
+			$wpiJavascriptSnippets[] = $script;
+
+			$revision = $wgRequest->getval('oldid');
+			$pvPwAdded[] = $pwId . '@' . $revision;
+
+			$pathway = Pathway::newFromTitle($pwId);
+			if($revision) {
+				$pathway->setActiveRevision($revision);
+			}
+			$svg = $pathway->getFileURL(FILETYPE_IMG);
+			$gpml = $pathway->getFileURL(FILETYPE_GPML);
+
+			$dostart = ',start: true';
+			$start = $dostart; //Autostart by default
+
+			//First switch by variable in pass.php
+			if(!$wpiAutoStartViewer) $start = $dostart;
+
+			//Allow override via get parameter
+			$getStart = $wgRequest->getval('startViewer');
+			if($getStart == 'false' || $getStart == '0') $start = '';
+			if($getStart == 'true' || $getStart == '1') $start = $dostart;
+
+			$script = <<<SCRIPT
+				var viewer = new PathwayViewer({
+					  imageId: "$imgId",
+							svgUrl: "$svg",
+							gpmlUrl: "$gpml"$start
+							});
+			PathwayViewer_viewers.push(viewer);
+SCRIPT;
+			$script = "<script type=\"{$wgJsMimeType}\">" . $script . "</script>\n";
+			return array($script, 'isHTML'=>1, 'noparse'=>1);
+		} catch(Exception $e) {
+			echo "<pre>"; debug_print_backtrace();
+			return "invalid pathway title: $e";
+		}
+		return true;
+	}
+
 	static function getJsDependencies() {
 		global $jsSvgWeb, $wgScriptPath, $wfPathwayViewerPath;
 
