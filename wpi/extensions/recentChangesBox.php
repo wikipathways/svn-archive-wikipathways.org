b55874334c525af5b4198798aefbfb36c2ada4da
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-09-14 16:48:37 +0000

    userCanRead is deprecated in favor of userCan(‘read')

--- wpi/extensions/recentChangesBox.php
+++ wpi/extensions/recentChangesBox.php
@@ -126,8 +126,7 @@ class RecentChangesBox {
 
 		$title = Title::newFromText($row->rc_title, $this->namespace);
 
-		if(!$title->userCanRead()) return ''; //Skip titles hidden for this user
-		//if(!$title->userCan('read')) return ''; //Skip titles hidden for this user
+		if(!$title->userCan('read')) return ''; //Skip titles hidden for this user
 
 		$perm = new PermissionManager($title->getArticleId());
 		if($perm->getPermissions()) {
