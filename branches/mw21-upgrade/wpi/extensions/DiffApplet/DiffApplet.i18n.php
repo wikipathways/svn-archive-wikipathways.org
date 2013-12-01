553d21319a925e389b6cee54855f3429e1a04aec
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-12-01 03:22:12 +0000

    Add aliases to avoid notices

--- branches/mw21-upgrade/wpi/extensions/DiffApplet/DiffApplet.i18n.php
+++ branches/mw21-upgrade/wpi/extensions/DiffApplet/DiffApplet.i18n.php
@@ -4,3 +4,13 @@ $messages = array(
 		'diffapplet' => 'Pathway Difference Viewer'
 	)
 );
+
+$specialPageAliases = array();
+
+/** English
+ * @author Mark A. Hershberger
+ */
+$specialPageAliases[ 'en' ] = array(
+		'DiffApplet' => array( 'DiffApplet', 'Pathway Difference Viewer' ),
+		'DiffAppletPage' => array( 'DiffAppletPage', 'Pathway Difference Viewer' ),
+);
