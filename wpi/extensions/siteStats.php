e99293c45ea071791a5c7e9029b33e8820c1c856
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-12-01 00:51:47 +0000

    Clean up siteStats output

--- wpi/extensions/siteStats.php
+++ wpi/extensions/siteStats.php
@@ -11,34 +11,28 @@ class wpiSiteStats {
 
 	static function getSiteStats( &$parser, $tableAttr ) {
 		$nrPathways = StatisticsCache::howManyPathways('total');
-		$output = "\n* There are '''{$nrPathways}''' pathways";
 
 		if( ! is_dir( WPI_CACHE_PATH ) && ! wfMkdirParents( WPI_CACHE_PATH ) ) {
 			wfDebug( "Can't create: " . WPI_CACHE_PATH );
 			throw new Exception( "Can't create WPI_CACHE_PATH!" );
 		}
 
-		$table = <<<EOD
-* Number of '''pathways''' ''(and unique genes)'' per species:
-{| align="center" $tableAttr
-EOD;
+		$table = "";
 		foreach(Pathway::getAvailableSpecies() as $species) {
 			$nr = StatisticsCache::howManyPathways($species);
 			$genes = StatisticsCache::howManyUniqueGenes($species);
 			if ($nr > 0) {  // skip listing species with 0 pathways
-				$table .= <<<EOD
-
-|-align="left"
-|$species:
-|'''$nr'''
-|''($genes)''
-EOD;
+				$table .= "\n| $species || '''$nr''' || ''($genes)''\n|-\n";
 			}
 		}
-		$table .= "\n|}";
-		$output .= $table;
-		$output .= "\n* There are '''{{NUMBEROFUSERS}}''' registered users";
-		//$output .= "\n* Active user [[Special:ContributionScores|statistics]]";
+		$output  = "\n* There are '''{$nrPathways}''' pathways.\n";
+		$output .= "* Number of '''pathways''' ''(and unique genes)'' per species:\n";
+		$output .= "{| align='center' $tableAttr\n" . $table . "\n|}\n";
+		$output .= "* There are '''{{NUMBEROFUSERS}}''' registered users\n";
+		$output .= "* There are '''{{NUMBEROFACTIVEUSERS}}''' active users";
+
+		// The reason we don't just return $output here is because we need
+		// to parse the magic words.
 		$output = $parser->recursiveTagParse( $output );
 		return array( $output, 'isHTML' => true, 'noparse' => true,
 			'nowiki' => true );
