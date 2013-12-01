7a0628c85b3b15dea7791e6b02f4da18be345bd4
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-12-01 01:00:06 +0000

    left over typos from the great class rename

--- branches/mw21-upgrade/wpi/extensions/DiffApplet/DiffApplet_body.php
+++ branches/mw21-upgrade/wpi/extensions/DiffApplet/DiffApplet_body.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class LegacyBrowsePathways extends LegacySpecialPage {
+class LegacyDiffApplet extends LegacySpecialPage {
 	function __construct() {
 		parent::__construct( "DiffAppletPage", "DiffApplet" );
 	}
